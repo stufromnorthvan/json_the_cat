7d99b374c11c91fb8226188f576b4c5231834d03
stufromnorthvan <stumitchell@irsss.ca>
2022-09-29 16:18:28 -0700

    breed fetcher exercise, completed with mocha testing available

---
+++ index.js
@@ -0,0 +1,12 @@
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const process = require('process');
+let breedName = process.argv.slice(2);
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
